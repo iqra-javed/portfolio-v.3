e011041a55588c8cf3f27e9d2a636d0451a4ade7
Iqra Javed <35740252+iqra-javed@users.noreply.github.com>
2018-10-20 14:14:14 -0500

    cleaned up code and styles

--- dist/js/slider.js
+++ dist/js/slider.js
@@ -1,57 +1,52 @@
-
-// var cssProperties = { left: '-200px', background: "red", display: "block" }
-var cssProperties = { left: '25%' }
-
-// var shapeToggle = {webkitTransform: 'rotateY(0deg)'}
-
-
-// $('#home').click(function() {
-//   $('.nav-content-block').animate(cssProperties);
-//   console.log("clicked")
-// });
-
-$('.home').click(function() {
-  
-  $('.overlay-shape').css('-webkit-transform', 'rotate(45deg)');
-  $('.global-overlay').css('width', '100%')
-  $('.header-nav').css('width', '330px')
-  $('.header-nav').css('left', 'inherit');
+$(".home").click(function() {
+  $(".overlay-shape").css("-webkit-transform", "rotate(45deg)");
+  $(".overlay-shape").css("transition", "transform 1s");
+  $(".global-overlay").css("width", "100%");
+  $(".header-nav").css("width", "330px");
+  $(".header-nav").css("left", "inherit");
 });
 
-$('.nav-tab').click(function() {
-  $('.header-nav').animate(cssProperties, 1000);
-  $('.overlay-shape').css('-webkit-transform', 'rotate(0deg)');
-  $('.header-nav').css('width', '25%');
-  $('.global-overlay').css('width', '50%');
-  $('.header-nav ul li a').animate({color: '#fff'});
+$(".nav-tab").click(function() {
+  $(".header-nav").animate({ left: "25%" }, 1000);
+  // $(".header-nav").css('left', '25%');
+  $(".overlay-shape").css("-webkit-transform", "rotate(0deg)");
+  $(".overlay-shape").css("transition", "transform 1s");
+  $(".header-nav").css("width", "25%");
+  $(".global-overlay").css("width", "50%");
 });
 
 $(".about").click(function() {
-    $([document.documentElement, document.body]).animate({
-        scrollTop: $("#about").offset().top
-    }, 1000);
+  $([document.documentElement, document.body]).animate(
+    {
+      scrollTop: $("#about").offset().top
+    },
+    1000
+  );
 });
 
 $(".skills").click(function() {
-    $([document.documentElement, document.body]).animate({
-        scrollTop: $("#skills").offset().top
-    }, 1000);
+  $([document.documentElement, document.body]).animate(
+    {
+      scrollTop: $("#skills").offset().top
+    },
+    1000
+  );
 });
 
 $(".projects").click(function() {
-    $([document.documentElement, document.body]).animate({
-        scrollTop: $("#projects").offset().top
-    }, 1000);
+  $([document.documentElement, document.body]).animate(
+    {
+      scrollTop: $("#projects").offset().top
+    },
+    1000
+  );
 });
 
 $(".contact").click(function() {
-    $([document.documentElement, document.body]).animate({
-        scrollTop: $("#contact").offset().top
-    }, 1000);
-});
-
-$(".resume").click(function() {
-  $([document.documentElement, document.body]).animate({
-      scrollTop: $("#resume").offset().top
-  }, 1000);
+  $([document.documentElement, document.body]).animate(
+    {
+      scrollTop: $("#contact").offset().top
+    },
+    1000
+  );
 });
